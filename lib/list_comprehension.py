bc75dfab01ae3d0c55c235f09352b866494ad0da
kwamebrowne <theeecryptoking@gmail.com>
2023-05-23 21:20:33 -0500

    python pair programming deuce

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    return [num for num in num_list if num % 2 == 0]
+
     pass
 
+# returns a [] if none
+
+
 def make_exclamation(sentence_list):
+    return [ i + "!" for i in sentence_list  ]
     pass
+
+# returns a [] if none
